99b39acf3d437d0dd54e4169034228586d066733
hk.li <lihangkong1986@gmail.com>
2013-09-13 14:25:24 +0800

    fix soap_action

--- lib/wash_out/router.rb
+++ lib/wash_out/router.rb
@@ -15,7 +15,9 @@ module WashOut
 
         if WashOut::Engine.namespace
           namespace = Regexp.escape WashOut::Engine.namespace.to_s
-          soap_action.gsub!(/^\"(#{namespace}(\/|#)?)?(.*)\"$/, '\3')
+          #soap_action.gsub!(/^\"(#{namespace}(\/|#)?)?(.*)\"$/, '\3')
+          soap_action.gsub!(/^\"(.*)\"$/, '\1')
+          soap_action.gsub!(/.*\//, '') # strip any namespace
         else
           soap_action = soap_action[1...-1]
         end
